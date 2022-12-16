fd908b339abff4405138e30182e74ec6c775b2ae
Ritwik Goel <ritwikgoel14@gmail.com>
2022-12-16 12:28:31 +0530

    Fix: Moved to SingleTon class for faster processing

--- src/main/java/vidsort/Main.java
+++ src/main/java/vidsort/Main.java
@@ -1,9 +1,6 @@
 package vidsort;
 
-import net.bramp.ffmpeg.FFmpeg;
-import net.bramp.ffmpeg.FFmpegExecutor;
-import net.bramp.ffmpeg.FFprobe;
-import net.bramp.ffmpeg.builder.FFmpegBuilder;
+import Processing.VideoCompress;
 import org.apache.log4j.BasicConfigurator;
 
 import java.io.IOException;
@@ -11,34 +8,9 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         BasicConfigurator.configure();
-        System.out.println("Hello World");
-        FFmpeg ffmpeg = new FFmpeg("/opt/homebrew/bin/ffmpeg");
-        FFprobe ffprobe = new FFprobe("/opt/homebrew/bin/ffprobe");//manual path
-        FFmpegBuilder builder = new FFmpegBuilder()
+//        System.out.println("Hello World");
+        VideoCompress.INSTANCE.compress();
 
-                //.setInput("/Users/ritwikgoel/Downloads/input.mp4")     // Filename, or a FFmpegProbeResult
-                .setInput("/Users/ritwikgoel/Downloads/input.mp4")//manual path as of now
-                .overrideOutputFiles(true) // Override the output if it exists
-                .addOutput("output.mp4")   // Filename for the destination
-                .setFormat("mp4")        // Format is inferred from filename, or can be set
-                .disableSubtitle()       // No subtiles
-                .setAudioChannels(1)         // Mono audio
-                .setAudioCodec("aac")        // using the aac codec
-                .setAudioSampleRate(48_000)  // at 48KHz
-                .setVideoCodec("libx264")     // Video using x264
-                .setVideoFrameRate(24, 1)     // at 24 frames per second
-                .setVideoResolution(640, 480) // at 640x480 resolution
-
-                .setStrict(FFmpegBuilder.Strict.EXPERIMENTAL) // Allow FFmpeg to use experimental specs
-                .done();
-
-        FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
-
-// Run a one-pass encode
-        executor.createJob(builder).run();
-
-// Or run a two-pass encode (which is better quality at the cost of being slower)
-        // executor.createTwoPassJob(builder).run();
 
     }
 }
