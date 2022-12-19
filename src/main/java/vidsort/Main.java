2882d3af039e050149abb7858059c759b3fc865c
Ritwik Goel <ritwikgoel@yahoo.com>
2022-12-19 23:49:46 +0530

    Changes

--- src/main/java/vidsort/Main.java
+++ src/main/java/vidsort/Main.java
@@ -11,9 +11,9 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BasicConfigurator.configure();
         MongoConn mongoConn= new MongoConn();
-        mongoConn.makeConnection();
+        //mongoConn.makeConnection();
 
-        //VideoCompress.INSTANCE.compress();
+        VideoCompress.INSTANCE.compress();
 
     }
 }
